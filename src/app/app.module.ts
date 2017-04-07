14feebbc14bdf19cc61b7b6400aeb2a45278c586
Jason Brown <jasontbrown99@gmail.com>
2017-04-07 09:29:20 -0700

    adjust title; add blazerservice; and adjust html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
+import { BlazerService } from './blazer.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
