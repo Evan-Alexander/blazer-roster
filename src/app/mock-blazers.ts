81aa45a0233943e759e46c13826965cce3892e21
Jason Brown <jasontbrown99@gmail.com>
2017-04-07 08:36:51 -0700

    create home component with routing; create mock-blazer file; add navbar to app.component; add module file

---
+++ src/app/mock-blazers.ts
@@ -0,0 +1,16 @@
+import { Blazer } from './blazer.model';
+
+export const BLAZERS: Blazer[] = [
+ new Blazer("Damian Lillard", "Point Guard",
+     "The leader of the team. Awarded rookie of the year his first season., 1),
+ new Blazer("CJ McCollum", "Shooting Guard",
+     "Awarded most improved player in the NBA is second season.  A shot creator and unbelievable ball handling skills.", 2),
+ new Blazer("Mo Harkless", "Small Forward",
+     "Came into his own during the 2015-16 season earning him a spot on the starting five.", 3),
+ new Blazer("AL Forouk Aminu", "Power Forward",
+     "Please dont take the ball up court.  Great defense and atheltic abilities.", 4),
+ new Blazer("Jusuf Nurkic", "Center",
+     "His second season in the NBA is looking to have some serious all-star potential.  The Blazers are an entirely different team with him on the court.", 5),
+ new Blazer("Allen Crabbe", "Shooting Guard",
+     "Knocks the light out of three-point shots", 6)
+];
