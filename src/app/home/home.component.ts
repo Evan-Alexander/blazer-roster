86e5c6080157bab7f870504afd4ea4a80b62d550
Jason Brown <jasontbrown99@gmail.com>
2017-04-07 10:40:17 -0700

    add async pipe and adjust to accept data from firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Blazer } from '../blazer.model';
 import { Router } from '@angular/router';
 import { BlazerService } from '../blazer.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-home',
@@ -11,16 +12,16 @@ import { BlazerService } from '../blazer.service';
 })
 
 export class HomeComponent implements OnInit {
-  blazers: Blazer[];
-  
+  blazers: FirebaseListObservable<any[]>;;
+
   constructor(private router: Router, private blazerService: BlazerService) { }
 
   ngOnInit() {
     this.blazers = this.blazerService.getBlazers();
   }
 
-  goToDetailPage(clickedBlazer: Blazer) {
-    this.router.navigate(['blazers', clickedBlazer.id]);
+  goToDetailPage(clickedBlazer) {
+    this.router.navigate(['blazers', clickedBlazer.$key]);
   }
 
 }
