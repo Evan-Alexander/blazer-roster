fe441ce3c4790fd16eadf6024b984c30629a50de
Jason Brown <jasontbrown99@gmail.com>
2017-04-07 09:28:22 -0700

    remove import of blazer service - need coffe

--- src/app/blazer.service.ts
+++ src/app/blazer.service.ts
@@ -5,10 +5,17 @@ import { BLAZERS } from './mock-blazers';
 @Injectable()
 export class BlazerService {
 
-  constructor(private router: Router, private blazerService: BlazerService) { }
+  constructor() { }
 
   getBlazers() {
     return BLAZERS;
   }
 
+  getBlazerById(blazerId: number){
+    for (var i = 0; i <= BLAZERS.length - 1; i++) {
+      if (BLAZERS[i].id === blazerId) {
+        return BLAZERS[i];
+      }
+    }
+  }
 }
