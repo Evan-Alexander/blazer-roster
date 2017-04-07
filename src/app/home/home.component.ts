495c1ad0997d40d171363a06ccf6a189250d152f
Jason Brown <jasontbrown99@gmail.com>
2017-04-07 10:58:41 -0700

    add if statement to display manager, if in that route; imbed edit-blazer html component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,8 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 
 export class HomeComponent implements OnInit {
-  blazers: FirebaseListObservable<any[]>;;
+  blazers: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private blazerService: BlazerService) { }
 
